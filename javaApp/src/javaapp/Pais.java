8bd9dfb32c0e9860ea46114da14db26b6332a492
Lijerim1 <63688867+Lijerim1@users.noreply.github.com>
2020-05-26 22:53:06 -0300

    Update Pais.java

--- javaApp/src/javaapp/Pais.java
+++ javaApp/src/javaapp/Pais.java
@@ -1,5 +1,38 @@
 package javaapp;
 
 public class Pais {
+private String nome;
+	private int idDD;
+	private ArrayList<Estado> listaEstados = new ArrayList<>();
+
+	public Pais(String nome, int codDDI) {
+		this.nome = nome;
+		this.codDDI = idDD;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getidDD() {
+		return idDD;
+	}
+
+	public void addEstado(Estado e) {
+		for (Estado estadoEncontrado : listaEstados) {
+			if (estadoEncontrado.getSigla().equals(e.getSigla())) {
+				return;
+			}
+		}
+		listaEstados.add(e);
+	}
+
+	public ArrayList<Estado> getEstados() {
+		return listaEstados;
+	}
+
+	public int contarEstados() {
+		return listaEstados.size();
+	}
 
 }
